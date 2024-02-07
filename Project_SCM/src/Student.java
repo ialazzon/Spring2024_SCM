c9760747fe88b4f95e96e02a7bf76da71302cb3a
Brahim <201910420@aau.ac.ae>
2024-02-07 14:37:21 +0400

    brahim2

--- Project_SCM/src/Student.java
+++ Project_SCM/src/Student.java
@@ -1,7 +1,11 @@
 
 public class Student {
 public static void main(String [] args) {
+<<<<<<< Upstream, based on origin/master
 	System.out.print("Issam Ahmed Mohammed!!");
+=======
+	System.out.print("Issam Ahmed Ali!!");
+>>>>>>> ae46c7d brahim2
 }
 }
  
