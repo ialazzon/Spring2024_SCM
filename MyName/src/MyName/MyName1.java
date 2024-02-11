3bcf4f2d5cd42bfff5e6e630d56c2a8dfa70d18a
Salmaaladwan <salmaaladwan2003@gmail.com>
2024-02-11 19:58:21 +0400

    added name

--- MyName/src/MyName/MyName1.java
+++ MyName/src/MyName/MyName1.java
@@ -3,8 +3,7 @@ package MyName;
 public class MyName1 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		System.out.println("My Name is Salma AL Adwan ");
 	}
 
 }
